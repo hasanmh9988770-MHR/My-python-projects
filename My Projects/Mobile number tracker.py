cc7ea8106af9f69513555db81031b0d478ff460c
mehedi hasan <hasanmh9988770@gmail.com>
2026-03-11 13:36:03 +0600

    seven number commit

---
+++ My Projects/Mobile number tracker.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Version: 1.0.0
+
+import phonenumbers
+from phonenumbers import timezone, carrier, geocoder
+
+number = input("Enter number to track (with or without +91): ")
+phone = phonenumbers.parse(number, "IN")  # Default region IN
+time = timezone.time_zones_for_number(phone)
+carr = carrier.name_for_number(phone, 'en')
+reg = geocoder.description_for_number(phone, 'en')
+
+print("STD Code: ", phone.country_code)
+print("Number: ", phone.national_number)
+print("Region: ", time)
+print("Carrier: ", carr)
+print("Country: ", reg)
+
+
+#+919876543210
